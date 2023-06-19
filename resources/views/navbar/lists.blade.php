e2de4853f981b7445cdcc2e1c53d2a9a5d7a9bf4
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2023-06-20 07:43:36 +0800

    Initial Navbar

--- resources/views/navbar/lists.blade.php
+++ resources/views/navbar/lists.blade.php
@@ -1,17 +1,19 @@
 <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-    <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-    </li>
-    <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-    </li>
-    <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-    </li>
-    <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-    </li>
-    <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-    </li>
+    @foreach(['Home', 'Members', 'About', 'Contact'] as $list)
+        <li>
+            <a href="#" aria-current="page"
+                class="{{
+                    (request()->is(strtolower($list))) ? 'text-blue-300' : 'text-white dark:text-white'
+                }}
+                block
+                py-2
+                pl-3
+                pr-4
+                rounded
+                md:p-0"
+                >
+                {{ $list}}
+            </a>
+        </li>
+    @endforeach
 </ul>
