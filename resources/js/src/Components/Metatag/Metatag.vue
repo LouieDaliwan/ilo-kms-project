8f04e4a201cdd0a4299174a197243d4392376e84
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2023-07-28 17:19:17 +0800

    Feat: Adding Metatag Component, and WIP statemanagement

---
+++ resources/js/src/Components/Metatag/Metatag.vue
@@ -0,0 +1,23 @@
+<script>
+import { defineStore } from "pinia";
+
+export default {
+    name: "Metatag",
+
+    props: ["title", "description"],
+
+    watch: {
+        title(val) {
+            defineStore("metatag/set", {
+                title: val,
+            });
+
+            document.title = val;
+        },
+        description(val) {},
+    },
+};
+</script>
+<template>
+    <keep-alive></keep-alive>
+</template>
--- resources/js/src/modules/Users/Create.vue
+++ resources/js/src/modules/Users/Create.vue
@@ -1,2 +1,2 @@
-<template></template>
 <script></script>
+<template></template>
