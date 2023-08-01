74453415a46c0cf419a8507478768331cb37c45f
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2023-08-01 20:46:28 +0800

    Feat: WIP on User Dashboard

---
+++ resources/js/src/Components/Empty/EmptyState.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-row align="center" justify="center">
+        <v-card class="text-center muted--text" color="transparent" flat>
+            <div class="icon--disabled">
+                <empty-icon class="primary--text"></empty-icon>
+            </div>
+            <h3 class="muted--text">{{ trans("No resource found") }}</h3>
+            <p class="muted--text mb-8">"This page returned empty results."</p>
+            <slot name="actions"></slot>
+        </v-card>
+    </v-row>
+</template>
+
+<style scoped>
+.icon--disabled {
+    filter: grayscale(0.8);
+}
+</style>
