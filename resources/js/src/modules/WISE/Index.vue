f66351191779b7337620ef029c3b909ed6a591f5
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2023-12-11 18:27:30 +0800

    WIP WISE

--- resources/js/src/modules/WISE/Index.vue
+++ resources/js/src/modules/WISE/Index.vue
@@ -1,2 +1,44 @@
-<script setup></script>
-<template></template>
+<script>
+import Admin from "@/Components/Layouts/Admin.vue";
+import PageHeader from "@/Components/Headers/PageHeader.vue";
+import MetaTag from "@/Components/Metatag/Metatag.vue";
+import { useDisplay } from "vuetify";
+
+export default {
+    name: "Dashboard",
+
+    components: {
+        Admin,
+        PageHeader,
+        MetaTag,
+    },
+
+    setup() {
+        const { smAndDown } = useDisplay();
+        return { smAndDown };
+    },
+};
+</script>
+
+<template>
+    <admin>
+        <meta-tag :title="'Dashboard'"></meta-tag>
+
+        <page-header>
+            <template v-slot:action>
+                <v-btn
+                    :block="!!smAndDown"
+                    :to="{ name: 'users.all' }"
+                    color="primary"
+                    large
+                    rounded
+                >
+                    <v-icon left small
+                        >mdi-file-document-box-search-outline
+                    </v-icon>
+                    {{ "All Users" }}
+                </v-btn>
+            </template>
+        </page-header>
+    </admin>
+</template>
