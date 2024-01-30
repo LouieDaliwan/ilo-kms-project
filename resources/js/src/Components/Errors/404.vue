0d2442dab2d977ccc32a399c0b89f05f01bd2bab
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2024-01-31 03:22:14 +0800

    Adding Content for error pages

--- resources/js/src/Components/Errors/404.vue
+++ resources/js/src/Components/Errors/404.vue
@@ -1,5 +1,45 @@
-<script setup></script>
-
 <template>
-    <h1>404</h1>
+    <v-app v-cloak class="dovetail-app" footer toolbar>
+        <v-slide-y-transition mode="out-in">
+            <v-container fill-height>
+                <v-col cols="12">
+                    <v-row align="center" justify="center">
+                        <v-col
+                            class="d-none d-md-flex order-md-2"
+                            cols="12"
+                            md="6"
+                            offset-md="1"
+                        >
+                            <open-map-icon
+                                class="primary--text"
+                            ></open-map-icon>
+                        </v-col>
+
+                        <v-col class="order-md-1" cols="12" md="5">
+                            <div class="text-center">
+                                <h3 class="text-uppercase muted--text mb-5">
+                                    <strong>404 Error</strong>
+                                </h3>
+                                <h2 class="mb-2">There's no page here 😭</h2>
+                                <p class="muted--text mb-5">
+                                    Looks like you ended up here by accident?
+                                </p>
+
+                                <v-btn
+                                    :to="{ name: 'dashboard' }"
+                                    class="mb-5"
+                                    color="primary"
+                                    exact
+                                    large
+                                    v-text="trans('Return to your dashboard')"
+                                ></v-btn>
+
+                                <!-- <superadmin-only></superadmin-only> -->
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-container>
+        </v-slide-y-transition>
+    </v-app>
 </template>
--- resources/js/src/Components/Errors/500.vue
+++ resources/js/src/Components/Errors/500.vue
@@ -5,18 +5,20 @@
                 <v-col cols="12">
                     <v-row align="center" justify="center">
                         <v-col cols="12" md="8">
-                            <h1 class="display-4 muted--text mb-5">403</h1>
-                            <h2 class="mb-3">Oops.. page is restricted.</h2>
+                            <h1 class="display-4 muted--text mb-5">500</h1>
+                            <h2 class="mb-3">
+                                Woopsie! That was very bad. Very bad, indeed.
+                            </h2>
                             <p classs="muted--text mb-5">
                                 We are sorry but you do not have permission to
                                 access this page
                             </p>
 
                             <v-btn
-                                :to="{ name: 'dashboard' }"
                                 color="primary"
                                 exact
                                 large
+                                to="dashboard"
                                 v-text="'Return to your dashboard'"
                             ></v-btn>
                         </v-col>
